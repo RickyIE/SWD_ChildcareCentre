c8c7f222ab72d53d0abcfc88614d57db74dd7a2d
RickyIE <alex.t.mlad@gmail.com>
2021-05-16 05:47:19 +0100

    bug
    bug

--- Testing/session3.php
+++ Testing/session3.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Location: https://".$_SERVER['HTTP_HOST']."  ---  ".rtrim(dirname($_SERVER['PHP_SELF']), '/\\')." --- /index.php";
+echo "Location: https://".$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\')."/index.php";
 
 
 ?>
