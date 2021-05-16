f36653d5b29b436ef575ed34b91fa82edb09b7ad
RickyIE <alex.t.mlad@gmail.com>
2021-05-16 06:47:53 +0100

    bug
    bug

--- login-form.php
+++ login-form.php
@@ -62,7 +62,7 @@ else
           $websiteURL = strval("Location: https://".$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\')."/index.php");
           $localPORT = strval("Location: index.php");
 
-        header("Location: https://www.meetalex.org/");
+        header("Location: https://www.google.com/");
 
         exit();        
       } else {
