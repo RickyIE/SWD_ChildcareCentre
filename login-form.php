31bb1c87385d6edef85bdb62d675c107067060b6
RickyIE <alex.t.mlad@gmail.com>
2021-05-16 07:55:19 +0100

    bug
    bug

--- login-form.php
+++ login-form.php
@@ -64,11 +64,11 @@ else
         // go to home page
 
           $websiteURL = strval("Location: https://".$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\')."/index.php");
-          $localPORT = strval("Location: ../index.php");
+          $localPORT = strval("Location: index.php");
           $location = $_SERVER['HTTP_HOST'];
           $pattern = "/localhost/i";
 
-            if (preg_match($pattern, $location) === 1 ){ // if running on local machine redirect locally else redirect web
+            if (preg_match($pattern, $location) == 1 ){ // if running on local machine redirect locally else redirect web
 
                 header($localPORT);
 
